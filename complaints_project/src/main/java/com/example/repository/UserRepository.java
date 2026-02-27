35fd76b929520a86c41a1fb732f3b67e2b3bb52b
Madiniramyasri0112 <2420030353@klh.edu.in>
2026-02-27 20:26:41 +0530

    Delete complaints_project directory

--- complaints_project/src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.model.user;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<user, Long> {
-    Optional<user> findByEmail(String email);
-}
